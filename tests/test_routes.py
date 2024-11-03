319ef8ddaab00bb11252ed466c55cf26e7a627aa
wilson b <wd.bracamonte10@gmail.com>
2024-11-03 17:40:10 -0500

    making correction in the format

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -170,10 +170,8 @@ class TestAccountService(TestCase):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-   
     def test_security_headers(self):
         """It should return security headers"""
-       
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         headers = {
@@ -184,7 +182,6 @@ class TestAccountService(TestCase):
         }
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
-    
     def test_cors_security(self):
         """It should return a CORS header"""
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
