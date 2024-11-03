019b496087afd5fdbd36189e3b02578d43b29b84
wilson b <wd.bracamonte10@gmail.com>
2024-11-03 18:55:01 -0500

    added deploy task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -144,11 +144,11 @@ class TestAccountService(TestCase):
         resp=self.client.get(BASE_URL)
         self.assertEqual(resp.status_code,status.HTTP_200_OK)
         data = resp.get_json()
-        self.assertEqual(len(data),5)
+        self.assertEqual(len(data), 5)
 
     def test_delete_account(self):
         """It should Delete an Account"""
-        account=self._create_accounts(1)[0]
+        account = self._create_accounts(1)[0]
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
 
@@ -170,6 +170,7 @@ class TestAccountService(TestCase):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+    
     def test_security_headers(self):
         """It should return security headers"""
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
@@ -182,6 +183,7 @@ class TestAccountService(TestCase):
         }
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
+    
     def test_cors_security(self):
         """It should return a CORS header"""
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
